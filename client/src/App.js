0ac997b9aeeb7fd868756b34f1f82803d1803e27
andrei <andrei@sampler.io>
2017-12-22 18:01:38 -0500

    Add simple material-ui grid and button, decruft, add onclick call to API

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Grid } from 'material-ui';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+
+        // Function bindings
+        this.refreshProduct = this.refreshProduct.bind(this);
+    }
+
+    async refreshProduct() {
+        let data = await (await fetch('http://localhost:3500/product')).json();
+        console.log(data);
+    }
+
+    render() {
+        return (
+            <div>
+                <Grid container spacing={24} justify="center">
+                    <Grid item xs={4}>
+                        <Button raised color="primary" className="randomize-button" onClick={this.refreshProduct}>Show me a random item</Button>
+                    </Grid>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default App;
